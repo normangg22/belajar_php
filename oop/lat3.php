8fb80b2b0a69bfa210a87a577246af66556f4603
unknown <norman>
2021-09-13 15:12:47 +0700

    oop

---
+++ oop/lat3.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="isi3.php" method="post">
+        <fieldset>
+            <legend>Aritmatika</legend>
+            <table>
+                <tr>
+                    <td>Bilangan 1 </td>
+                    <td>:</td>
+                    <td><input type="number" name="bilangan1"></td>
+                </tr>
+                <tr>
+                    <td>Bilangan 2 </td>
+                    <td>:</td>
+                    <td><input type="number" name="bilangan2"></td>
+                </tr>
+                <tr>
+                    <td></td><td></td>
+                    <td><input type="submit" name="proses" value="Proses"></td>
+                    <td><input type="reset" name="reset" value="reset"></td>
+                </tr>
+            </table>
+        </fieldset>
+    </form>
+</body>
+</html>
